c17efebd79871c8b1c8614abba480c5c0d5a41a5
Casual player <emailofgmz@gmail.com>
2023-04-30 16:25:20 +0800

    Test

--- script/ngaCookis.js
+++ script/ngaCookis.js
@@ -1,27 +1,17 @@
-// ==UserScript==
-// @name         Get NGA Cookie
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Get NGA Cookie for Surge
-// @author       Your Name
-// @match        https://bbs.nga.cn/*
-// @grant        none
-// ==/UserScript==
+const cookieName = 'NGAcookie'
 
-(function() {
-    'use strict';
-    var cookieValue = '';
-    if(document.cookie.length > 0){
-        var cookieStart = document.cookie.indexOf("ngaPassportUid=");
-        if(cookieStart !== -1){
-            cookieStart = cookieStart + "ngaPassportUid=".length;
-            var cookieEnd = document.cookie.indexOf(";", cookieStart);
-            if(cookieEnd === -1){
-                cookieEnd = document.cookie.length;
-            }
-            cookieValue = decodeURIComponent(document.cookie.substring(cookieStart, cookieEnd));
+$httpClient.get('https://ngabbs.com', function(error, response, data){
+    if (error) {
+        $notification.post("获取NGAcookie失败", error, "")
+        $done()
+    } else {
+        var cookie = response.headers["Set-Cookie"]
+        if (cookie) {
+            $persistentStore.write(cookie, cookieName)
+            $notification.post("获取NGAcookie成功", "", "")
+        } else {
+            $notification.post("获取NGAcookie失败", "未找到cookie", "")
         }
+        $done()
     }
-    $notification.post('获取cookie成功', cookieValue);
-    $done({headers: {'Cookie': 'ngaPassportUid=' + cookieValue}});
-})();
+})
