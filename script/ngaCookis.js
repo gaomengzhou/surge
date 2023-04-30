ae2cbe490fae6744c5e3ba6741c2362c74138854
Casual player <emailofgmz@gmail.com>
2023-04-30 16:18:39 +0800

    Test

--- script/ngaCookis.js
+++ script/ngaCookis.js
@@ -3,11 +3,10 @@ let myCookie = '';
 
 // 定义请求头信息
 const headers = {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1',
-    'Accept-Language': 'zh-cn',
-    'Cookie': 'your_cookie_here'
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16A366",
+    "Content-Type": "application/octet-stream",
+    "Cookie": "your_cookie_here"
 };
-alert('test')
 // 发送请求获取cookie
 $httpClient.get('https://ngabbs.com', {headers: headers}, function(error, response, data){
     if (error) {
